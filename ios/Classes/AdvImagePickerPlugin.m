26d59a9a5e3d226549a912add968eda2f0412883
Thanh Truong <truongthanhcn12@gmail.com>
2020-02-27 21:08:50 +0700

    Update AdvImagePickerPlugin.m

--- ios/Classes/AdvImagePickerPlugin.m
+++ ios/Classes/AdvImagePickerPlugin.m
@@ -1,5 +1,12 @@
 #import "AdvImagePickerPlugin.h"
-#import <adv_image_picker-Swift.h>
+#if __has_include(<adv_image_picker/adv_image_picker-Swift.h>)
+#import <adv_image_picker/adv_image_picker-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "adv_image_picker-Swift.h"
+#endif
 
 @implementation AdvImagePickerPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
